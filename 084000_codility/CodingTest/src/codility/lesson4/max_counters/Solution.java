54e248833a8fca8b58561f7bff8b3772db3fd360
InmookJeong <jeonginmook@gmail.com>
2022-06-13 20:40:37 +0900

    fix: codility level 4 maxcounters 100%

--- 084000_codility/CodingTest/src/codility/lesson4/max_counters/Solution.java
+++ 084000_codility/CodingTest/src/codility/lesson4/max_counters/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	public static void main(String[] args) {
 		int N = 5;
 		int[] A = {3, 4, 4, 6, 1, 4, 4};
-		int[] returnA = solution1(N, A);
+		int[] returnA = solution2(N, A);
 		for(int subA : returnA) {
 			System.out.println(subA);
 		}
@@ -18,12 +18,33 @@ public class Solution {
 		int max = 0;
 		int[] counters = new int[N];
 		for(int subA : A) {
-			if(subA == (N+1)) Arrays.fill(counters, max);
+			if(subA > N) Arrays.fill(counters, max);
 			else {
 				counters[subA-1] += 1;
 				max = Math.max(max, counters[subA-1]);
 			}
 		}
+		return counters;
+	}
+	
+	// Total 100%
+	public static int[] solution2(int N, int[] A) {
+		int max = 0;
+		int temp = 0;
+		int[] counters = new int[N];
+		
+		for(int subA : A) {
+			if(subA > N) max = temp;
+			else {
+				if(counters[subA-1] < max) counters[subA-1] = max;
+				counters[subA-1]++;
+				if(counters[subA-1] > temp) temp = counters[subA-1];
+			}
+		}
+		for(int i=0; i<counters.length; i++) {
+			if(counters[i] < max) counters[i] = max;
+		}
+		
 		return counters;
 	}
 }
